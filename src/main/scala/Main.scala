dc2050442a4b5a7055b85495d0d41cb3a4d933a5
Alex Grejuc <grejuca@oregonstate.edu>
2022-04-09 21:17:22 -0700

    add aes encryptor with hardcoded key and iv

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,12 @@
 package filesharer
 
+import filesharer.encryptor.Encryptor
+
 object Main {
+  // assuming 3 command line arguments `f` `e` `d`:
+  // saves encrypted version of file `f` to `e` and decrypted version to `d`
   def main(args: Array[String]) = {
-    println("Hello, world")
+    Encryptor.encryptFileTo(args(0), args(1))
+    Encryptor.decryptFileTo(args(1), args(2))
   }
 }
---
+++ src/main/scala/encryptor/Encryptor.scala
@@ -0,0 +1,60 @@
+package filesharer
+package encryptor
+
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
+
+object Encryptor {
+  val key = "0123456789ABCDEF".getBytes("UTF-8")
+  val initVector = "0123456789ABCDEF".getBytes("UTF-8")
+
+  def encryptFileTo(from: String, to: String): Unit = {
+    cryptFileTo(encryptFile, from, to)
+  }
+
+  def decryptFileTo(from: String, to: String): Unit = {
+    cryptFileTo(decryptFile, from, to)
+  }
+
+  def cryptFileTo(f: (FileInputStream, FileOutputStream) => Unit, from: String, to: String): Unit = {
+    val src = new File(from)
+    val dst = new File(to)
+    dst.createNewFile()
+
+    val srcs = new FileInputStream(src)
+    val dsts = new FileOutputStream(dst)
+
+    f(srcs, dsts)
+  }
+
+  def encryptFile(in: FileInputStream, out: FileOutputStream): Unit = {
+    val encrypted = encrypt(in.readAllBytes())
+    out.write(encrypted)
+  }
+
+  def encrypt(bytes: Array[Byte]): Array[Byte] = {
+    val iv = new IvParameterSpec(initVector)
+    val skeySpec = new SecretKeySpec(key, "AES")
+
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv)
+
+    cipher.doFinal(bytes)
+  }
+
+  def decryptFile(in: FileInputStream, out: FileOutputStream): Unit = {
+    val decrypted = decrypt(in.readAllBytes())
+    out.write(decrypted)
+  }
+
+  def decrypt(bytes: Array[Byte]): Array[Byte] = {
+    val iv = new IvParameterSpec(initVector)
+    val skeySpec = new SecretKeySpec(key, "AES")
+
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv)
+    cipher.doFinal(bytes)
+  }
+}
