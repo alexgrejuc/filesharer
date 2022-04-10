468523f6b407a5ac35fc0a5c0751604ffdac038a
Alex Grejuc <grejuca@oregonstate.edu>
2022-04-10 13:00:32 -0700

    remove dead code

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -7,18 +7,6 @@ import scala.io.BufferedSource
 import encryptor.Encryptor
 
 object Client {
-  def run(): Unit = {
-    val s = new Socket(InetAddress.getByName("localhost"), 9999)
-    lazy val in = new BufferedSource(s.getInputStream()).getLines()
-    val out = new PrintStream(s.getOutputStream())
-
-    out.println("Client is running")
-    out.flush()
-    println("Received: " + in.next())
-
-    s.close()
-  }
-
   def send(fileName: String): Unit = {
     println("Client attempting connection")
 
